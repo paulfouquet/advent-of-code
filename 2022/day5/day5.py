33c21358ed064c6cbe4cd324a3834a61112fea9d
Paul Fouquet <pfouquet@linz.govt.nz>
2022-12-06 16:27:37 +1300

    feat(2022): day5 part2

--- 2022/day5/day5.py
+++ 2022/day5/day5.py
@@ -26,6 +26,34 @@ def main():
 
         print(top_list)
 
+    with open("input", "r") as f:
+        # Part 2
+        stacks, procedures = map(str.splitlines, f.read().rstrip().split("\n\n"))
+        col = stacks[len(stacks) - 1]
+        stack_columns = {}
+
+        for stack in reversed(stacks[:-1]):
+            for char_position, element in enumerate(stack):
+                if element.isalpha():
+                    if not stack_columns.get(col[char_position]):
+                        stack_columns[col[char_position]] = []
+                    stack_columns[col[char_position]].append(element)
+
+        for procedure in procedures:
+            number, ori, dst = map(int, re.findall(r"\d+", procedure))
+            crates = []
+            for _ in range(0, number):
+                print(stack_columns)
+                crates.insert(0, stack_columns[str(ori)].pop())
+            for crate in crates:
+                stack_columns[str(dst)].append(crate)
+
+        top_list = ""
+        for stack in stack_columns.values():
+            top_list += stack.pop()
+
+        print(top_list)
+
 
 if __name__ == "__main__":
     main()
